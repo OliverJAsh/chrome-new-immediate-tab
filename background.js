4737bbcc3ef0db5d29b225b939886de00b31ab13
Oliver Joseph Ash <oliverjash@gmail.com>
2016-09-21 20:59:16 +0100

    Persist current

--- background.js
+++ background.js
@@ -1,14 +1,15 @@
-let current;
 chrome.tabs.onActivated.addListener(activeInfo => {
-    current = activeInfo;
+    chrome.storage.sync.set({ current: activeInfo })
 });
 
 const newImmediateTab = () => {
-    if (current) {
-        chrome.tabs.get(current.tabId, tab => {
-            chrome.tabs.create({ index: tab.index + 1 })
-        })
-    } else throw new Error('No current tab.');
+    chrome.storage.sync.get('current', ({ current }) => {
+        if (current) {
+            chrome.tabs.get(current.tabId, tab => {
+                chrome.tabs.create({ index: tab.index + 1 })
+            })
+        } else throw new Error('No current tab.');
+    })
 }
 chrome.commands.onCommand.addListener(command => {
     if (command === 'new-immediate-tab') {
